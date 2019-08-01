97f97d20afa724687c3bcde19428c7f8c1baf211
DMW007 <danielw@posteo.de>
2019-08-01 11:43:08 +0200

    New project structure created to seperate the parser from the main community repo

---
+++ ULabs.BBCodeParser/BBCodeNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ULabs.BBCodeParser {
+    public class BBCodeNode {
+        public string TagName { get; set; }
+        public string OpenTag { get; set; }
+        public string Argument { get; set; }
+        public string CloseTag { get; set; }
+        public bool IsClosed {
+            get => !string.IsNullOrEmpty(CloseTag);
+        }
+        public bool HasTag {
+            get => !string.IsNullOrEmpty(TagName);
+        }
+
+        public string InnerContent { get; set; }
+        public string InnerHtml { get; set; }
+        public List<BBCodeNode> Childs { get; set; }
+
+        public override string ToString() {
+            string str = $"{OpenTag}{InnerContent}";
+            if (IsClosed) {
+                str += CloseTag;
+            }
+            return str;
+        }
+    }
+}
---
+++ ULabs.BBCodeParser/BBCodeNodePosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ULabs.BBCodeParser {
+    public class BBCodeNodePosition {
+        public int OpenTagStart { get; set; }
+        public int OpenTagEnd { get; set; }
+
+        public int CloseTagStart { get; set; }
+        public int CloseTagEnd { get; set; }
+
+        public BBCodeNode Node { get; set; }
+    }
+}
